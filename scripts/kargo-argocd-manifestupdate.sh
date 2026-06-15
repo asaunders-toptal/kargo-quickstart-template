b5444b1ebec2e539ffd65c3ca1ab10a8f6a0236b
asaunders-toptal <allen.saunders@toptal.com>
2026-06-15 19:12:42 +0000

    changed stages

--- scripts/kargo-argocd-manifestupdate.sh
+++ scripts/kargo-argocd-manifestupdate.sh
@@ -3,5 +3,5 @@ set -e
 
 for stage in $(kargo get stages --project kargo-simple -o jsonpath='{.items[*].metadata.name}')
 do
-	kargo get stage ${stage} -p kargo-simple -o json | jq ".spec.promotionMechanisms.argoCDAppUpdates[0] += {\"appName\": \"guestbook-simple-${stage}\"}" | kargo apply -f -
+	kargo get stage "${stage}" -p kargo-simple -o json | jq --arg stage "${stage}" '.spec.promotionMechanisms.argoCDAppUpdates[0].appName = $stage' | kargo apply -f -
 done
